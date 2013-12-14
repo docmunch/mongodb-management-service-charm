ca08d76931c1ade5796b63ed669332a4bcd7a6c4
Max Cantor (MBPr) <i@cantor.mx>
2013-12-13 18:13:11 -0800

    almost working

--- precise/mongodb-management-service/scripts/common-variables.sh
+++ precise/mongodb-management-service/scripts/common-variables.sh
@@ -1,12 +1,12 @@
 SVC_NAME="mmsagent"
-SVC_AUTHOR="Max Cantor <nax@docmunch.com>"
+SVC_AUTHOR="Max Cantor <max@docmunch.com>"
 SVC_USER=${SVC_NAME}
 
 
 MMS_URL="https://mms.mongodb.com/settings/mms-monitoring-agent.tar.gz"
 MMS_HOMEDIR="/srv/mongo-management-service/agent"
 MMS_UNPACKED_DIR="mms-agent"
-MMS_HOME="${MMS_HOME}/${MMS_UNPACKED_DIR}"
+MMS_HOME="${MMS_HOMEDIR}/${MMS_UNPACKED_DIR}"
 
 HOSTS_FILE="/etc/hosts"
 
